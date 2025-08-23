da728a1bbfa7ba4140805d9bfb3224f4056bb4bd
Electricmantaray <hlsj1066@gmail.com>
2025-08-23 01:59:47 +0100

    feat(Algorithms): Merge sort implimentation
    Top-dow implimentation of the merge sort algorithm

--- Data-Structures-&-Algorithms/1. Merge-Sort.py
+++ Data-Structures-&-Algorithms/1. Merge-Sort.py
@@ -1,6 +1,9 @@
 """
 Algorithm: Merge Sort
 
+Sources:
+    - https://en.wikipedia.org/wiki/Merge_sort
+
 Description:
     The data is repeatedly split in 
     half until each item is its own list.
@@ -25,7 +28,7 @@ Examples:
         1.[[3], [3], [1], [5], [2], [3], [4], [4], [2], [7], [1], [9]]
         2.[[3, 3], [1, 5], [2, 3], [4, 4], [2, 7], [1, 9]]
         3.[[1, 3, 3, 5], [2, 3, 4, 4], [1, 2, 7, 9]]
-        4.[[1, 2, 3, 3, 3, 4, 4, 5], [1, 2, 7, 9]] # Odd number of lists retains the last list
+        4.[[1, 2, 3, 3, 3, 4, 4, 5], [1, 2, 7, 9]]
         5.[1, 1, 2, 2, 3, 3, 3, 4, 4, 5, 7, 9]
 
         Solved
@@ -61,3 +64,69 @@ Time & Space Complexity:
         Worst:   O(n)
 
 """
+
+# Imports
+from timeit import default_timer as timer
+
+
+# Global input which all solutions use
+items = list(map(int, input("Enter numbers separated by space: ").split()))
+
+
+# Main implimentations for the merge sort algorithm
+class mergeSortTopDown:
+    """Recursive algorithm"""
+    def mergeSort(self, l):
+        # A list of 0 or 1 elements is sorted by default
+        if len(l) <= 1:
+            return l
+        
+        
+        # Divide the list into equal size sub-lists
+        # left = first half, right = second half
+        
+        left, right = [], []
+
+        for i, v in enumerate(l):
+            if i < (len(l))//2:
+                left.append(v)
+            else:
+                right.append(v)
+        
+        left, right = self.mergeSort(left), self.mergeSort(right)
+
+        return self.merge(left, right)
+    
+    def merge(self, left, right):
+        result = []
+
+        # Checks if left and right are not empty
+        while (left) and (right):
+            # Compares element 1 of left and right, smaller element appends to result
+            if left[0] <= right[0]:
+                result.append(left[0])
+                left = left[1:]
+            else:
+                result.append(right[0])
+                right = right[1:]
+        
+        # Checks for remaining elements
+        while (left):
+            result.append(left[0])
+            left = left[1:] 
+        while (right):
+            result.append(right[0])
+            right = right[1:]
+        
+        return result
+
+
+# Benchmarking the algorithms
+
+# Top-down
+print("#########################")
+start = timer()
+print("Top-down merge sort: ", mergeSortTopDown().mergeSort(items))
+end = timer()
+print("Execution time: ", end - start, "seconds")
+print("#########################")
