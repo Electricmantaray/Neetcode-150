9e0b07573059dee62e5b00ece68ec58fc3495c4a
Electricmantaray <hlsj1066@gmail.com>
2025-09-02 17:07:54 +0100

    feat(Neetcode): Documentation for the top k frequent problem

---
+++ Arrays-And-Hashing/5. Top-K-Frequent-Elements-(Medium).py
@@ -0,0 +1,30 @@
+"""
+Problem: https://neetcode.io/problems/top-k-elements-in-list?list=neetcode150
+
+Description:
+    Given an integer array nums and 
+    an integer k, return the k most 
+    frequent elements within the array.
+
+    The test cases are generated such 
+    that the answer is always unique.
+
+    You may return the output in any order.
+    (Allowed to utilise unstable algorithm)
+
+Examples:
+    Example 1:
+        Input: nums = [1,2,2,3,3,3], k = 2
+
+        Output: [2,3]
+
+    Example 2:
+        Input: nums = [7,7], k = 1
+
+        Output: [7]
+
+            
+Recommended Time & Space Complexity:
+    Time:   O(n)
+    Space:  O(n)
+"""
