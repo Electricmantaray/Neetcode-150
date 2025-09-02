1c60ea83d04ab6d81cd7384c42f9dfd03a79a1d5
Electricmantaray <hlsj1066@gmail.com>
2025-09-02 16:51:46 +0100

    docs(Neetcode): Documentation for the two sum problem

---
+++ Arrays-And-Hashing/3. Two-Sum-(Easy).py
@@ -0,0 +1,31 @@
+"""
+Problem: https://neetcode.io/problems/two-integer-sum?list=neetcode150
+
+Description:
+    Given an array of integers nums and 
+    an integer target, return the indices 
+    i and j such that 
+    nums[i] + nums[j] == target and i != j.
+
+    You may assume that every input has exactly 
+    one pair of indices i and j that satisfy 
+    the condition.
+
+    Return the answer with the smaller index first. 
+
+Examples:
+    Example 1:
+        Input: nums = [3,4,5,6], target = 7
+
+        Output: [0,1]
+
+    Example 2:
+        Input: nums = [4,5,6], target = 10
+
+        Output: [0,2]
+
+
+Recommended Time & Space Complexity:
+    Time:   O(n)
+    Space:  O(n)
+"""
